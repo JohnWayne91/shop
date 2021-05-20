0407774411fa699e9cbceec4dddfd30ab8296c5f
Vladislav <vladislav.sytyuk@gmail.com>
2021-05-20 17:49:50 +0300

    connected to postgres and migrate

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'mainapp.apps.MainappConfig'
 ]
 
 ROOT_URLCONF = 'shop.urls'
@@ -75,8 +76,8 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop', 'USER': 'postgres', 'PASSWORD': '2635', 'HOST': 'localhost', 'PORT': '5432',
     }
 }
 
