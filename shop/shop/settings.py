04561f2cee1572fc9353332093caafac8e9cd259
Vladislav <vladislav.sytyuk@gmail.com>
2021-05-20 18:14:21 +0300

    resolved issue with runserver

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainapp.apps.MainappConfig'
 ]
 
 MIDDLEWARE = [
@@ -47,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'mainapp'
 ]
 
 ROOT_URLCONF = 'shop.urls'
