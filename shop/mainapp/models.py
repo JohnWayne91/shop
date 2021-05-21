34a739576771c76b9ff6d9cb5f576bb0c15167c9
Vladislav <vladislav.sytyuk@gmail.com>
2021-05-21 17:49:12 +0300

    add product models

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -63,5 +63,29 @@ class Customer(models.Model):
         return f'Customer: {self.user.first_name} {self.user.last_name}'
 
 
+class Notebook(Product):
+    diagonal = models.CharField(max_length=255, verbose_name='Screen diagonal')
+    display_type = models.CharField(max_length=255, verbose_name='display matrix type')
+    processor_freq = models.CharField(max_length=255, verbose_name='Processor frequency')
+    ram = models.CharField(max_length=255, verbose_name='RAM')
+    video = models.CharField(max_length=255, verbose_name='Video card')
+    time_without_charge = models.CharField(max_length=255, verbose_name='Time-running without charging')
 
+    def __str__(self):
+        return f'{self.category.name} : {self.title}'
+
+
+class Smartphone(Product):
+    diagonal = models.CharField(max_length=255, verbose_name='Screen diagonal')
+    display_type = models.CharField(max_length=255, verbose_name='display matrix type')
+    resolution = models.CharField(max_length=255, verbose_name='Resolution')
+    battery_volume = models.CharField(max_length=255, verbose_name='Battery volume')
+    ram = models.CharField(max_length=255, verbose_name='RAM')
+    sd = models.BooleanField(default=True)
+    sd_memory_max = models.CharField(max_length=255, verbose_name='Maximum memory capacity')
+    main_cam_mp = models.CharField(max_length=255, verbose_name='Main camera')
+    front_cam_mp = models.CharField(max_length=255, verbose_name='Front camera')
+
+    def __str__(self):
+        return f'{self.category.name} : {self.title}'
 
