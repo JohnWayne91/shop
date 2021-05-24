583570f731df874baf560b5f089cefeddfe5e5c5
Vladislav <vladislav.sytyuk@gmail.com>
2021-05-24 17:35:18 +0300

    add restrictions on resolution of uploading images

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,10 +1,27 @@
-from django.forms import ModelChoiceField
+from django.forms import ModelChoiceField, ModelForm, ValidationError
 from django.contrib import admin
+from PIL import Image
 
 from .models import *
 
 
+class NotebookAdminForm(ModelForm):
+    MIN_RESOLUTION = (400, 400)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = "Download images with minimal resolution {}x{}".format(*self.MIN_RESOLUTION)
+
+    def clean_image(self):
+        image = self.cleaned_data('image')
+        img = Image.open(image)
+        min_height, min_width = self.MIN_RESOLUTION
+        if img.height < min_height or img.width < min_width:
+            raise ValidationError('The resolution of the uploaded image is less than the minimum allowed')
+
+
 class NotebookAdmin(admin.ModelAdmin):
+    form = NotebookAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
