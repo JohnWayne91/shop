45478ed5d40423a2fe0234adddd72b3566226d30
Vladislav <vladislav.sytyuk@gmail.com>
2021-07-02 12:21:22 +0300

    Add tenplate and view for specifications

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -22,7 +22,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('mainapp.api.urls')),
-    path('', include('mainapp.urls'))
+    path('', include('mainapp.urls')),
+    path('product-specs/', include('specs.urls'))
 ]
 
 if settings.DEBUG:
